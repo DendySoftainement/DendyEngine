fc1ea77fda1e9cd7992637d275828140501c7ce4
DendyTheElephant <danielhuc7@gmail.com>
2017-12-07 00:44:29 +0100

    No tabs, use spaces

--- code/DendyFoundation/Types.h
+++ code/DendyFoundation/Types.h
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// -----------------------------------------------------------------------------------------------------------------------------------------------------------
 ////   \file    Types.h
-////   \author	 Daniel Huc aka Dendy
+////   \author  Daniel Huc aka Dendy
 ////   \date    December 2017
 ////   -----------------------------
 ////   \brief   Aliases for types
@@ -48,17 +48,17 @@ namespace DendyEngine {
 
          /* Substitution Aliases */
          using dyBool = bool;             /// 1b boolean GL_TRUE or GL_FALSE
-         using dyInt16 = int_fast16_t;	   /// Signed 16b integer [-32 768,  32 767] at least! (can be more if faster on system)
-         using dyUInt16 = uint_fast16_t;	/// Unsigned 16b integer [0,  65 535] at least! (can be more if faster on system)
+         using dyInt16 = int_fast16_t;    /// Signed 16b integer [-32 768,  32 767] at least! (can be more if faster on system)
+         using dyUInt16 = uint_fast16_t;  /// Unsigned 16b integer [0,  65 535] at least! (can be more if faster on system)
          using dyInt = int_fast32_t;      /// Signed 32b integer [-2 147 483 648,  2 147 483 647] at least! (can be more if faster on system)
          using dyInt32 = int32_t;         /// Signed 32b integer [-2 147 483 648,  2 147 483 647] STRICT (usefull for third party libs)
          using dyUInt32 = uint32_t;       /// Unsigned 32b integer [0,  4 294 967 295] STRICT (usefull for third party libs)
          using dyUInt = uint_fast32_t;    /// Unsigned 32b integer [0,  4 294 967 295] at least! (can be more if faster on system)
          using dyInt64 = int_fast64_t;    /// Signed 64b integer [-9 223 372 036 854 775 808,  9 223 372 036 854 775 807] at least! (can be more if faster
                                           ///    on system)
-         using dyUInt64 = uint_fast64_t;	/// Unsigned 64b integer [0,  18 446 744 073 709 551 615] at least! (can be more if faster on system)
-         using dyFloat = float;				/// Signed 32b floating-point
-         using dyFloat64 = double;			/// Signed 64b floating-point
+         using dyUInt64 = uint_fast64_t;  /// Unsigned 64b integer [0,  18 446 744 073 709 551 615] at least! (can be more if faster on system)
+         using dyFloat = float;           /// Signed 32b floating-point
+         using dyFloat64 = double;        /// Signed 64b floating-point
 
          using dyString = std::string;
          template<class T, class U>
