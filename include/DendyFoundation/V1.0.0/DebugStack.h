b07084e2e2318418b3fc8e7690b488d17eeda714
DendyTheElephant <danielhuc7@gmail.com>
2018-04-23 18:59:49 +0200

    Trying new GUI Git client

---
+++ include/DendyFoundation/V1.0.0/DebugStack.h
@@ -0,0 +1,163 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+////   \file    DebugStack.h
+////   \author  Daniel Huc aka Dendy
+////   \date    December 2017
+////   -----------------------------
+////   \brief   None
+////   ---------------------------------------------------------------------------------------------------------------------------------------------------------
+////   \details None
+//// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// FIXME: -
+////        -
+//// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+//// TODO:  - FINISH
+////        - TEST
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+//// - Standard includes section - ////
+#include <stack>
+
+//// - External includes section - ////
+
+//// -Foundation includes section- ////
+#include "DendyFoundation/Types.h"
+#include "DendyFoundation/InputOutput/OutputStreamInterface.h"
+
+//// - Internal includes section - ////
+
+//// - Defines and macro section - ////
+#ifndef _DENDYENGINE_DEFINEGUARD_DEBUG_SYSTEM
+
+   // Double macro to display the number of the line instead of "__LINE__"
+   #define _DENDYENGINE_DISPLAY_VALUE_HELPER_SECOND_PASS(value) #value
+   #define _DENDYENGINE_DISPLAY_VALUE_HELPER(value) _DENDYENGINE_DISPLAY_VALUE_HELPER_SECOND_PASS(value)
+
+   // Define _DENDYENGINE_CURRENT_FUNCTION macro to call __func__ define for c++11 gcc or __FUNCTION__ defined for MSVC!
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Specific to 'WINDOWS/LINUX' <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+   #ifdef DENDYENGINE_PLATFORM_WINDOWS
+      #define _DENDYENGINE_CURRENT_FUNCTION __FUNCTION__
+   #endif
+   #ifdef DENDYENGINE_PLATFORM_LINUX
+      #define _DENDYENGINE_CURRENT_FUNCTION _DENDYENGINE_DISPLAY_VALUE_HELPER(__PRETTY_FUNCTION__)
+   #endif
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+   // Stack display message (file:function:line)
+   #define _DENDYENGINE_LOCATION ( __FILE__ "#" _DENDYENGINE_CURRENT_FUNCTION "#" _DENDYENGINE_DISPLAY_VALUE_HELPER(__LINE__) )
+
+   ////////////////////////////////////////////////////////////
+   // The actually public interface for the Debug system !!! //
+   ////////////////////////////////////////////////////////////
+   #ifdef DENDYENGINE_MODE_DEBUG
+      #define DENDYENGINE_CALLSTACK_ENTER         (&DendyEngine::DendyFoundation::DebugTools::UDebugStack::getInstance())->enter(_DENDYENGINE_LOCATION)
+      #define DENDYENGINE_CALLSTACK_EXIT          (&DendyEngine::DendyFoundation::DebugTools::UDebugStack::getInstance())->exit()
+      #define DENDYENGINE_LOG(message)            (&DendyEngine::DendyFoundation::DebugTools::UDebugStack::getInstance())->log(message,_DENDYENGINE_LOCATION)
+      #define DENDYENGINE_ERROR(message)          (&DendyEngine::DendyFoundation::DebugTools::UDebugStack::getInstance())->error(message,_DENDYENGINE_LOCATION)
+      #define DENDYENGINE_CRITICAL_ERROR(message) (&DendyEngine::DendyFoundation::DebugTools::UDebugStack::getInstance())->criticalError(message,_DENDYENGINE_LOCATION)
+   #else
+      #define DENDYENGINE_CALLSTACK_ENTER         (/***/)
+      #define DENDYENGINE_CALLSTACK_EXIT          (/***/)
+      #define DENDYENGINE_LOG(message)            (/***/)
+      #define DENDYENGINE_ERROR(message)          (/***/)
+      #define DENDYENGINE_CRITICAL_ERROR(message) (/***/)
+   #endif
+   ////////////////////////////////////////////////////////////
+
+   #define _DENDYENGINE_DEFINEGUARD_DEBUG_SYSTEM
+#endif
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//// ---- Namespaces ---- ////
+namespace DendyEngine {
+
+   namespace DendyFoundation {
+
+      namespace DebugTools {
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////   Class CDebugMessage                                                                                                                               ////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+         class UDebugStack {
+
+         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         //// ---- Enum/Struct/Constants -----                                                                                                   ////
+         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         private:
+            enum class EEventType {
+               DY_BEGIN,
+               DY_END,
+               DY_LOG,
+               DY_ERROR,
+			   DY_CRITICAL_ERROR
+            };
+
+            struct SCodeLocation {
+               dyString fileStr;
+               dyString functionStr;
+               dyString lineStr;
+               SCodeLocation( dyString rawLocationStr = "" );
+            };
+
+            struct SDebugEvent {
+               EEventType    type;
+               dyString      messageStr;
+               SCodeLocation context;
+               SDebugEvent():
+                  type( EEventType::DY_LOG ),
+                  messageStr( "" ),
+                  context( "" )
+                     {/***/};
+            };
+
+         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         //// ---- Members -----                                                                                                                 ////
+         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         private:
+            std::stack<SDebugEvent> m_stack;
+            dyInt m_currentLevel;
+
+         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         //// ---- Methods -----                                                                                                                 ////
+         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         private:
+         //// ---- Internal ---- ////
+            void _dump();
+            void _flush();
+
+         public:
+         //// ----  Object  ---- ////
+
+         //// ----  Static  ---- ////
+            static UDebugStack& getInstance();
+            static void destroyInstance();
+
+         //// ---- Accessor ---- ////
+
+         //// ----   Core   ---- ////
+            void enter( dyString locationStr );
+            void log( dyString messageStr, dyString locationStr );
+            void exit();
+            void error( dyString messageStr, dyString locationStr );
+            void criticalError( dyString messageStr, dyString locationStr );
+
+         };
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////                                                                                                                                                     ////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      }
+   }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ include/DendyFoundation/V1.0.0/Types.h
@@ -0,0 +1,107 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+////   \file    Types.h
+////   \author  Daniel Huc aka Dendy
+////   \date    December 2017
+////   -----------------------------
+////   \brief   Aliases for types
+////   ---------------------------------------------------------------------------------------------------------------------------------------------------------
+////   \details Using standard types may cause confusion in memory representation (depending on system) \n
+////            Plus if some mathematical functions will be changed in the future, we want to avoid multiple modifications!
+//// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// FIXME: -
+////        -
+//// -----------------------------------------------------------------------------------------------------------------------------------------------------------
+//// TODO:  -
+////        -
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+//// - Standard includes section - ////
+#include <vector>
+#include <memory>
+#include <map>
+#include <sstream>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Specific to 'WINDOWS' <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#ifdef DENDYENGINE_PLATFORM_WINDOWS
+   #include <stdint.h> ///< Using [basictype]_fast[minsize]_t
+#endif
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//// - External includes section - ////
+#define GLM_FORCE_RADIANS
+#include <glm/gtc/type_ptr.hpp>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//// ---- Namespaces ---- ////
+namespace DendyEngine {
+
+   /* Substitution Aliases */
+   using dyBool = bool;             /// 1b boolean GL_TRUE or GL_FALSE
+   using dyInt16 = int_fast16_t;    /// Signed 16b integer [-32 768,  32 767] at least! (can be more if faster on system)
+   using dyUInt16 = uint_fast16_t;  /// Unsigned 16b integer [0,  65 535] at least! (can be more if faster on system)
+   using dyInt = int_fast32_t;      /// Signed 32b integer [-2 147 483 648,  2 147 483 647] at least! (can be more if faster on system)
+   using dyInt32 = int32_t;         /// Signed 32b integer [-2 147 483 648,  2 147 483 647] STRICT (usefull for third party libs)
+   using dyUInt32 = uint32_t;       /// Unsigned 32b integer [0,  4 294 967 295] STRICT (usefull for third party libs)
+   using dyUInt = uint_fast32_t;    /// Unsigned 32b integer [0,  4 294 967 295] at least! (can be more if faster on system)
+   using dyInt64 = int_fast64_t;    /// Signed 64b integer [-9 223 372 036 854 775 808,  9 223 372 036 854 775 807] at least! (can be more if faster
+                                    ///    on system)
+   using dyUInt64 = uint_fast64_t;  /// Unsigned 64b integer [0,  18 446 744 073 709 551 615] at least! (can be more if faster on system)
+   using dyFloat = float;           /// Signed 32b floating-point
+   using dyFloat64 = double;        /// Signed 64b floating-point
+
+   using dyString = std::string;
+   template<class T, class U>
+   using dyMap = std::map<T, U>;
+   template<class T>
+   using dyVec = std::vector<T>;
+   template<class T, class U>
+   using dyPair = std::pair<T,U>;
+   template<class T>
+   using dyUniquePtr = std::unique_ptr<T>;
+   template<class T>
+   using dySharedPtr = std::shared_ptr<T>;
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Specific to 'LINUX' <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#ifdef DENDYENGINE_PLATFORM_LINUX
+   template<typename T, typename... Args>
+   std::unique_ptr<T> createUniquePtr(Args&&... args) {
+       return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+   }
+#endif
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Specific to 'WINDOWS' <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#ifdef DENDYENGINE_PLATFORM_WINDOWS
+   template<typename T, typename... Args>
+   std::unique_ptr<T> createUniquePtr(Args&&... args) {
+       return std::make_unique<T>(std::forward<Args>(args)...);
+   }
+#endif
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+   template<class T>
+   dyBool is_lexically_castable( dyString const& a_string ) {
+   	std::stringstream ss(a_string);
+   	T result;
+   	if ((ss >> result).fail() || !(ss >> std::ws).eof())
+   		return false;
+   	return true;
+   }
+
+   template<class T>
+   T lexical_cast( dyString const& a_string ) {
+   	std::stringstream ss(a_string);
+   	T result;
+   	ss >> result;
+   	return result;
+   }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
